b4ef482ae2baf5aef36275ebac88b642b2be56f3
Bharath Srinath <bharathsrinath2110@gmail.com>
2024-09-29 16:35:11 +0530

    established routes for auth, profile and request and created a model for connection

--- src/app.js
+++ src/app.js
@@ -1,98 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const cookieParser = require("cookie-parser");
+
 app.use(express.json());
-const User = require("./models/user");
+app.use(cookieParser());
 
-// Signup route
-app.post("/signup", async (req, res) => {
-  try {
-    validateSignUpData(req);
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-    const { firstName, lastName, emailId, password } = req.body;
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    });
-
-    await user.save();
-    res.send("User Added successfully!");
-  } catch (err) {
-    res.status(400).send("ERROR : " + err.message);
-  }
-});
-
-// Get a single user by email
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
-
-  try {
-    console.log(userEmail);
-    const user = await User.findOne({ emailId: userEmail });
-    if (!user) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong ");
-  }
-});
-
-// Get all the users from the database
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send("Something went wrong ");
-  }
-});
-
-// Detele a user from the database
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const user = await User.findByIdAndDelete({ _id: userId });
-    //const user = await User.findByIdAndDelete(userId);
-
-    res.send("User deleted successfully");
-  } catch (err) {
-    res.status(400).send("Something went wrong ");
-  }
-});
-
-// Update data of the user
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("Skills cannot be more than 10");
-    }
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    console.log(user);
-    res.send("User updated successfully");
-  } catch (err) {
-    res.status(400).send("UPDATE FAILED:" + err.message);
-  }
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -36,9 +36,9 @@ const userSchema = new mongoose.Schema({
   },
   gender: {
     type: String,
-    validate(value) {
-      if (!["male", "female", "others"].includes(value))
-        throw new Error("Gender data is not valid!");
+    enum: {
+      values: ["male", "female", "other"],
+      message: `{VALUE} is not a valid gender type`,
     },
   },
   photoURL: {
@@ -62,6 +62,29 @@ const userSchema = new mongoose.Schema({
     }
   },
 },{timestamps:true});
+
+userSchema.methods.getJWT = async function () {
+  const user = this;
+
+  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+
+  return isPasswordValid;
+};
+
 
 const User = mongoose.model("User", userSchema);
 
